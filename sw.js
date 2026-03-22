65c43df4eccf42433103027b361fd6ffd446a749
Eli <eli@local>
2026-03-22 18:23:04 +0200

    gh-pages: קבצי PWA בשורש לפרסום GitHub Pages

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'noya-v1';
+const ASSETS = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'Noya.png',
+  'https://cdn.jsdelivr.net/npm/mathjs@13/package.json',
+  'https://unpkg.com/nerdamer/nerdamer.core.js',
+  'https://unpkg.com/nerdamer/Calculus.js',
+  'https://unpkg.com/nerdamer/Algebra.js',
+  'https://unpkg.com/nerdamer/Solve.js',
+  'https://cdn.plot.ly/plotly-2.35.2.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
